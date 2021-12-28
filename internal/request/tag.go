80c75e4feb96db1bea5b84855dfaf987d9d91898
Will Yin <52946059+will-yinchengxin@users.noreply.github.com>
2021-12-28 10:30:44 +0800

    Update tag.go

--- internal/request/tag.go
+++ internal/request/tag.go
@@ -16,6 +16,9 @@ type OnlineList struct {
 	//StartTime int64 `form:"startTime" json:"startTime" validate:"required,unixTime" label:"开始时间"`
 	//EndTime   int64 `form:"endTime" json:"endTime" validate:"required,unixTime,gtfield=StartTime" label:"截止时间"`
 
+	Type       int64  `json:"type" validate:"required,oneof=1 2"`
+	Day        int64  `json:"day" validate:"required_if=Type 2,gte=0,lte=365"`  // 字段关联输入, required 和 required_if
+	
 	ChannelId string `form:"channelId" json:"channelId" validate:"omitempty" label:"渠道id"`
 	AppId     int64  `form:"appId" json:"appId" validate:"omitempty,gt=0" label:"应用id"`
 	Url       string `form:"url" json:"url" validate:"omitempty,urlFormat"` // 使用自定义校验规则
